91dfdfbe77e7d36e90791b7242892be3d35dd9bc
KotaSantolas <martimsantos@icloud.com>
2025-11-19 14:14:34 +0000

    Reset

--- api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const mysql = require('mysql2');
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-app.get('/', (req, res) => res.json({ message: 'Boas!' }));
